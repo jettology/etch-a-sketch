957144760c18d9b7153443f63aaedc9a6aa23a3d
Jettology <bsi.s.park@gmail.com>
2023-02-12 17:08:11 -0600

    Add standard draw, erase, clear functions

--- index.js
+++ index.js
@@ -9,36 +9,155 @@
 // Divs
 // ********************
 const screenContainerDiv = document.getElementById("screen-container");
+const cellDiv = document.getElementsByClassName(".cell");
+
+// ********************
+// Buttons
+// ********************
+const brushButton = document.getElementById("brush-button");
+const eraserButton = document.getElementById("eraser-button");
+const colorButton = document.getElementById("color-button");
+const clearButton = document.getElementById("clear-button");
+const inputButton = document.getElementById("input-button");
+
+// ********************
+// Slider
+// ********************
+const slider = document.getElementById("slider");
+const sliderText = document.getElementById("slider-text");
+
+// ********************
+// Grid
+// ********************
+
+// 2, 4, 6, 8, 10, ... just evens
+let numRows = 16;
+let numCols = (numRows * 1.5);
+let numCells = numRows * numCols;
+
+// ********************
+// Colors
+// ********************
+let overColor = "black";
+const defaultColor = "white";
+let currentColor = "black";
+let prevBrushColor = "black";
+let brushColor = "black";
+
+// ********************
+// Mouse
+// ********************
+let mousePressed = false;
 
 
-function makeGrid() {
+// ======================================================================
+// Event Listeners
+// ======================================================================
 
-    screenContainerDiv.style.gridTemplateRows = "repeat(10, 50px)";
-    screenContainerDiv.style.gridTemplateColumns = "repeat(10, 50px)";
+// check if mouse button is pressed down
+window.addEventListener('mouseup', () => mousePressed = false);
 
-    let cells = [];
+// clear button
+clearButton.addEventListener('click', clearGrid);
 
-    for (let i = 0; i < 70; i++) {
+// brush button
+brushButton.addEventListener('click', turnBrushOn);
+
+// eraser button
+eraserButton.addEventListener('click', turnEraserOn);
+
+
+// ======================================================================
+// Functions
+// ======================================================================
+
+// remove all divs from grid
+function removeDivs() {
+    screenContainerDiv.innerHTML = "";
+}
+
+// make a grid of divs based on row and col
+function makeGrid(row, col) {
+
+    numCells = row * col;
+
+    screenContainerDiv.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+    screenContainerDiv.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+
+    for (let i = 0; i < numCells; i++) {
         const newCell = makeCell();
-        cells.push(newCell);
+        newCell.addEventListener("mouseover", eventCell);
+        newCell.addEventListener("mouseout", eventCell);
+        newCell.addEventListener("mousedown", eventCell);
 
-        const newContent = document.createTextNode(`${i+1}`);
-        newCell.appendChild(newContent);
 
         screenContainerDiv.appendChild(newCell);
     }
-
-
 }
 
+// create a new div cell
 function makeCell() {
     const newDiv = document.createElement("div");
     newDiv.className = "cell";
-    newDiv.style.minHeight = "50px";
-    newDiv.style.width = "50px";
-    newDiv.style.border = "1px solid blue";
+    newDiv.style.border = "0.5px solid lightgray";
 
     return newDiv;
 }
 
-makeGrid();
+// adjust slider text when slider input gets changed
+slider.oninput = function() {
+    sliderText.innerHTML = `${this.value * 1.5} x ${this.value}`;
+
+    removeDivs();
+
+    numRows = this.value;
+    numCols = this.value * 1.5;
+    makeGrid(numRows, numCols);
+}
+
+// events related to mouse and grid interactions
+function eventCell(e) {
+    // if mouse hovers over cell, highlight it
+    if (e.type === "mouseover" && !mousePressed) {
+        currentColor = e.target.style.background;
+        e.target.style.background = overColor;
+    }
+    else if (e.type === "mouseover" && mousePressed) {
+        currentColor = brushColor;
+        e.target.style.background = brushColor;
+    }
+
+    // if mouse clicks over cell, color it
+    if (e.type === "mousedown") {
+        mousePressed = true;
+        currentColor = brushColor;
+        e.target.style.background = brushColor;
+    }
+
+    // if mouse moves out of cell, return to original color
+    if (e.type === "mouseout") {
+        e.target.style.background = currentColor;
+    }
+}
+
+// clear grid of all user inputs
+function clearGrid() {
+    removeDivs();
+    makeGrid(numRows, numCols);
+}
+
+// turn brush on
+function turnBrushOn() {
+    brushColor = prevBrushColor;
+    overColor = brushColor;
+}
+
+// turn brush into eraser
+function turnEraserOn() {
+    prevBrushColor = brushColor;
+    brushColor = "white";
+    overColor = brushColor;
+}
+
+
+makeGrid(16, 24);
